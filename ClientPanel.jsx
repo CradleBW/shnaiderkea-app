38e9731e2afc2132463a7e363692e58f8dc2a73d
CradleBW <bw_sm@ukr.net>
2025-06-13 00:40:42 +0200

    Delete ClientPanel.jsx

--- ClientPanel.jsx
+++
@@ -1,3 +0,0 @@
-// Короткая версия: кнопки назад и добавить ещё
-<button onClick={() => navigate(-1)}>← Назад</button>{' '}
-<button onClick={() => window.location.reload()}>+ Добавить ещё материал</button>
