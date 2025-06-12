fdc247edaf23c36c0caa420f790242d9edf6c413
CradleBW <bw_sm@ukr.net>
2025-06-12 15:04:50 +0200

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import AdminPanel from './AdminPanel'
+import ClientPanel from './ClientPanel'
 
 function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Добро пожаловать в ShnaiderKea!</h1>
-      <p><Link to="/admin">Перейти в панель администратора</Link></p>
+      <p><Link to="/admin">Панель администратора</Link></p>
+      <p><Link to="/client">Клиентская зона</Link></p>
     </div>
   )
 }
@@ -17,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/client" element={<ClientPanel />} />
       </Routes>
     </Router>
   )
