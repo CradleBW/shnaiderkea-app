e19b781a4af6602879f8da84ee9edd64d4534d3b
CradleBW <bw_sm@ukr.net>
2025-06-12 15:16:17 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import AdminPanel from './AdminPanel'
-import ClientPanel from './ClientPanel'
+import ClientPanel from './ClientPanel.jsx'
 
 function Home() {
   return (
