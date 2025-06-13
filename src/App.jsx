bdbe73c5b47a2f743cecd919b15683aedfd38cd6
CradleBW <bw_sm@ukr.net>
2025-06-13 21:37:20 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <div style={{ padding: 40, textAlign: 'center' }}>
+      <h1>Добро пожаловать в ShnaiderKea</h1>
+      <p><Link to="/client">Перейти в клиентскую панель</Link></p>
+      <p><Link to="/admin">Перейти в панель администратора</Link></p>
+    </div>
+  )
+}
