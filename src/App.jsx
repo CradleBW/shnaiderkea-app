ef75e28ada58bf45ffa33951e0893adac5c1f397
CradleBW <bw_sm@ukr.net>
2025-06-13 19:55:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 export default function App() {
-  return <h1>Добро пожаловать в ShnaiderKea</h1>
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Добро пожаловать в ShnaiderKea</h1>
+      <p><Link to="/client">Перейти в клиентскую панель</Link></p>
+      <p><Link to="/admin">Перейти в панель администратора</Link></p>
+    </div>
+  )
 }
