e94b40be66eb671b541931ee03c07b77ad3a693f
CradleBW <bw_sm@ukr.net>
2025-06-13 19:34:45 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    host: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
