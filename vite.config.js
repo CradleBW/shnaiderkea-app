0429b42ecc2bfa32d34db4bb68062945697530c2
CradleBW <bw_sm@ukr.net>
2025-06-14 10:18:34 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  publicDir: 'public',
-  server: { host: true },
-  build: { outDir: 'dist' }
-})
